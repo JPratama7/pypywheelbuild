36ae4dce897a1fc27b3aadcf87ec390b59374e72
Jose Chasey Pratama <85815288+JPratama7@users.noreply.github.com>
2021-12-17 22:27:40 +0800

    tesy

--- file.sh
+++ file.sh
@@ -1,6 +1,6 @@
 Linklist=(
-    "https://files.pythonhosted.org/packages/ce/d4/b2e4e5a18b1b1b7de159f28494a0b320c9624c354f82bcd135306a4c6c87/cvzone-1.5.3.tar.gz"
     "https://files.pythonhosted.org/packages/bc/b6/e447d0302bc328f06b56dabd6742648372df63446725db56058a259dbda2/opencv-python-4.5.4.60.tar.gz"
+    "https://files.pythonhosted.org/packages/ce/d4/b2e4e5a18b1b1b7de159f28494a0b320c9624c354f82bcd135306a4c6c87/cvzone-1.5.3.tar.gz"
 )
 
 for f in "${Linklist[@]}"; do
